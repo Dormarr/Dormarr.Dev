e7ed934510cefb004d460b6797d92bebc234f788
Dormarr <theryanappleyard@gmail.com>
2025-06-09 21:45:26 +0100

    Formatting and stuff. Refactoring and breaking out code

--- index.php
+++ index.php
@@ -4,34 +4,50 @@
     <link href="style.css"  rel="stylesheet">
     <title id="tabTitle">Dormarr.Dev</title>
 </head>
-<body>
+<body style="overflow: hidden">
     <div id="msg-box" class="message-container"></div>
-    <h1>Stinky buttholes</h1>
-    <h2>More stinky buttholes</h2>
-    <p>Something something eat poo</p>
-    <div class="slice" style="justify-content: space-around;">
-        <div>
-            <a href="pages/quay.html">Quay</a>
-        </div>
-        <div>
-            <a href="pages/tacks.html">Tacks</a>
-        </div>
-        <div>
-            <a>Something else</a>
+    <div style="height: 210px;">
+        <pre id="ascii" class="ascii" style="width: 100%; height: 210px;"></pre>
+        <div style="position: absolute; top: 48px; left: 32px;">
+            <h1>Dormarr.Dev</h1>
+            <p>I'm working on it. Leave me alone.</p>
         </div>
     </div>
-    <div>
-        <div class="square" style="background-color: var(--cerise);"></div>
-        <div class="square" style="background-color: var(--burnt-sienna);"></div>
-        <div class="square" style="background-color: var(--earth-yellow);"></div>
-    </div>    
+    <div class="line"></div>
+    <div class="dir" style="justify-content: space-around;">
+        <div>
+            <a href="pages/quay.html">> Quay <</a>
+        </div>
+        <div>
+            <a href="pages/tacks.html">> Tacks <</a>
+        </div>
+        <div>
+            <a style="text-decoration: line-through;">Something else</a>
+        </div>
+    </div>
+    <div class="line"></div>
+    <div style="display: flex; flex-direction: row; width: vw; justify-content: space-around; padding: 16px;">
+        <div class="square" style="border: 2px solid maroon;  width: 60%; height: fit-content;">
+            <h3>Devlogs</h3>
+            <p>I'll add some PHP widgets and stuff here</p>
+        </div>
+        <div class="square" style="border: 2px solid blueviolet; width: 40%; height: fit-content;">
+            <img src="../images/PFP.png" width="248px" height="auto" style="max-width: 100%">
+            <br>
+            <p>It's me.</p>
+        </div>
+    </div>
+
     <?php include 'shared/footer.php';?>
 </body>
 </html>
-<script src="utils.js"></script>
+<script src="../js/utils.js"></script>
+<script src="../js/perlin.js"></script>
 <script>
 
-const maxMessages = 5;
+const FPS = 1000 / 30;
+
+const maxMessages = 7;
 const container = l("msg-box");
 
 
@@ -67,7 +83,28 @@ function postNewMessage(){
     }
 }
 
+function renderFrame() {
 
+    let output = "";
+    const width = userScreen.clientWidth / 6;
+
+    if(width == 0) console.log("Issue with width of Perlin ASCII at index.php");
+    const height = 14;
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const value = perlin(x * 0.1, y * 0.1, t);
+        output += mapValueToChar(value);
+      }
+      output += "\n";
+    }
+
+    userScreen.textContent = output;
+    t += 0.01;
+  
+}
+
+setInterval(renderFrame, FPS);
 setInterval(postNewMessage, 7000)
 
 </script>
---
+++ js/perlin.js
@@ -0,0 +1,65 @@
+// #region Perlin
+const perm = new Uint8Array(512);
+for (let i = 0; i < 256; i++) perm[i] = i;
+for (let i = 255; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [perm[i], perm[j]] = [perm[j], perm[i]];
+}
+for (let i = 0; i < 256; i++) perm[i + 256] = perm[i];
+
+function fade(t) {
+  return t * t * t * (t * (t * 6 - 15) + 10);
+}
+
+function lerp(a, b, t) {
+  return a + t * (b - a);
+}
+
+function grad(hash, x, y, z) {
+  const h = hash & 15;
+  const u = h < 8 ? x : y;
+  const v = h < 4 ? y : h === 12 || h === 14 ? x : z;
+  return ((h & 1) === 0 ? u : -u) + ((h & 2) === 0 ? v : -v);
+}
+
+function perlin(x, y, z) {
+  const X = Math.floor(x) & 255;
+  const Y = Math.floor(y) & 255;
+  const Z = Math.floor(z) & 255;
+
+  x -= Math.floor(x);
+  y -= Math.floor(y);
+  z -= Math.floor(z);
+
+  const u = fade(x);
+  const v = fade(y);
+  const w = fade(z);
+
+  const A  = perm[X] + Y, AA = perm[A] + Z, AB = perm[A + 1] + Z;
+  const B  = perm[X + 1] + Y, BA = perm[B] + Z, BB = perm[B + 1] + Z;
+
+  return lerp(
+    lerp(
+      lerp(grad(perm[AA], x, y, z),
+           grad(perm[BA], x - 1, y, z), u),
+      lerp(grad(perm[AB], x, y - 1, z),
+           grad(perm[BB], x - 1, y - 1, z), u), v),
+    lerp(
+      lerp(grad(perm[AA + 1], x, y, z - 1),
+           grad(perm[BA + 1], x - 1, y, z - 1), u),
+      lerp(grad(perm[AB + 1], x, y - 1, z - 1),
+           grad(perm[BB + 1], x - 1, y - 1, z - 1), u), v),
+    w
+  );
+}
+
+const chars = "    ..:oO0@ ";
+const userScreen = l("ascii");
+let t = 0;
+let paused = false;
+
+function mapValueToChar(val) {
+  const normalized = (val + 1) / 2;
+  const index = Math.floor(normalized * (chars.length - 1));
+  return chars[index];
+}
