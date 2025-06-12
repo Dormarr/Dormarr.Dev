98507a9f054e9d389f690d680f6abe0c44a6c9d4
Dormarr <theryanappleyard@gmail.com>
2025-06-12 22:16:22 +0100

    Achievements system added

--- js/utils.js
+++ js/utils.js
@@ -1,2 +1,3 @@
 function l(what){ return document.getElementById(what);}
 function choose(arr){ return arr[Math.floor(Math.random()*arr.length)]; }
+function randomFloor(x) {if ((x%1)<Math.random()) return Math.floor(x); else return Math.ceil(x);}
